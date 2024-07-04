efda3ff81e2a3708848f7698b20de71e0af746cb
Manoj <manoj.gupta16539@sakec.ac.in>
2024-07-04 23:56:34 +0530

    chore: Refactor server.js and remove commented code

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
 console.log("server log is running")
+
+// function add(a,b) {
+//     return a+b;
+// }
+// var add = (a,b) =>   {return a+b};
+// var result = add(2,4);
+
+// console.log(result);
+
+// var subtract = (a,b) => a-b;
+// var result = subtract(9,4);
+
+// console.log(result);
+
+function callback() {
+    console.log("callback function is running");
+}
+
+const add = function(a, b, callback) {
+    var result = a + b;
+    console.log("result " + result)
+    callback();
+}
+
+add(2, 45, callback)
