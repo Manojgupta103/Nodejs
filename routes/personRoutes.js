5fec39f69cb8e65f9efe2da0502096d7d5811354
Manoj <manoj.gupta16539@sakec.ac.in>
2024-07-28 01:00:45 +0530

    chore: Add PUT and DELETE endpoints for /person in personRoutes.js

--- routes/personRoutes.js
+++ routes/personRoutes.js
@@ -45,5 +45,42 @@ router.post("/", async (req, res) => {
     }
   })
 
+  router.put('/:id', async (req, res)=> {
+    try{
+        const personId = req.params.id;
+        const updatePersonData = req.body;
+
+        const response = await Person.findByIdAndUpdate(personId, updatePersonData, {
+            new: true,
+            runValidators: true
+        })
+        if (!response) {
+            return res.status(404).json({ error: 'Person not found' });
+        }
+        console.log('data updated');
+        res.status(200).json(response);
+    }
+    catch(err){
+      console.log(err);
+      res.status(500).json({ error: "Error updating person" });
+    }
+  })
+
+  router.delete('/:id', async (req, res)=> {
+    try{
+        const personId = req.params.id;
+        const response = await Person.findByIdAndDelete(personId);
+        if (!response) {
+            return res.status(404).json({ error: 'Person not found' });
+        }
+        console.log('data deleted');
+        res.status(200).json({message: 'person Deleted Succesfully'});
+    }
+    catch(err){
+      console.log(err);
+      res.status(500).json({ error: "Error deleting person" });
+    }
+  })
+
 
   module.exports = router;
