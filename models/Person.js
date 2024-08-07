4878b721684c1facf8c6c666a979d7385cddb823
Manoj <manoj.gupta16539@sakec.ac.in>
2024-08-07 23:36:46 +0530

    chore: Refactor Person and Menu models and update schemas

--- models/Person.js
+++ models/Person.js
@@ -1,77 +1,77 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Define the Person schema
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    age:{
-        type: Number
-    },
-    work:{
-        type: String,
-        enum: ['chef', 'waiter', 'manager'],
-        required: true
-    },
-    mobile:{
-        type: String,
-        required: true
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: true
-    },
-    address:{
-        type: String
-    },
-    salary:{
-        type: Number,
-        required: true
-    },
-    username: {
-        required: true,
-        type: String
-    },
-    password: {
-        required: true,
-        type: String
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+  },
+  work: {
+    type: String,
+    enum: ["chef", "waiter", "manager"],
+    required: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+  username: {
+    required: true,
+    type: String,
+  },
+  password: {
+    required: true,
+    type: String,
+  },
 });
 
-personSchema.pre('save', async function(next){
-    const person = this;
+personSchema.pre("save", async function (next) {
+  const person = this;
 
-    // Hash the password only if it has been modified (or is new)
-    if(!person.isModified('password')) return next();
+  // Hash the password only if it has been modified (or is new)
+  if (!person.isModified("password")) return next();
 
-    try{
-        // hash password generation
-        const salt = await bcrypt.genSalt(10);
+  try {
+    // hash password generation
+    const salt = await bcrypt.genSalt(10);
 
-        // hash password
-        const hashedPassword = await bcrypt.hash(person.password, salt);
-        
-        // Override the plain password with the hashed one
-        person.password = hashedPassword;
-        next();
-    }catch(err){
-        return next(err);
-    }
-})
+    // hash password
+    const hashedPassword = await bcrypt.hash(person.password, salt);
 
-personSchema.methods.comparePassword = async function(candidatePassword){
-    try{
-        // Use bcrypt to compare the provided password with the hashed password
-        const isMatch = await bcrypt.compare(candidatePassword, this.password);
-        return isMatch;
-    }catch(err){
-        throw err;
-    }
-}
+    // Override the plain password with the hashed one
+    person.password = hashedPassword;
+    next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+personSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    // Use bcrypt to compare the provided password with the hashed password
+    const isMatch = await bcrypt.compare(candidatePassword, this.password);
+    return isMatch;
+  } catch (err) {
+    throw err;
+  }
+};
 
 // Create Person model
-const Person = mongoose.model('Person', personSchema);
+const Person = mongoose.model("Person", personSchema);
 module.exports = Person;
